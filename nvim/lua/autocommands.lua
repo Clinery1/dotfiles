6d2c0b9d75bfe87351dfef53ffaaca89ea2c8d1d
Clinery1 <clinery8237@gmail.com>
2022-06-03 01:07:47 -0400

    Update nvim configs to have a textwidth for markdown

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -9,15 +9,7 @@ vim.cmd("autocmd BufNewFile,BufRead,VimEnter,BufEnter *.gd  set ft=gdscript noex
 vim.cmd("autocmd BufEnter,BufRead *.c map <A-r> :execute 'let file=\"' expand('%:t') '\"'<CR>:w<CR>:tabedit<CR>:execute \"terminal ~/.bin/run_c_file --quiet \" file<CR>i")
 -- run a C++ file
 vim.cmd("autocmd BufEnter,BufRead *.cpp map <A-r> :execute 'let file=\"' expand('%:t') '\"'<CR>:w<CR>:tabedit<CR>:execute \"terminal ~/.bin/run_cpp_file --quiet \" file<CR>i")
--- convert a markdown file to a PDF file
-vim.cmd("autocmd BufEnter,BufRead *.md map <A-b> :execute 'let file=\"' expand('%:t') '\"'<CR>:w<CR>:tabedit<CR>:execute \"terminal md_to_pdf \" file<CR>i")
-vim.cmd([[
-augroup Markdown
-    au!
-    au BufReadPre  *.md set spell wrap linebreak nofoldenable
-    au BufReadPost *.md set spell wrap linebreak nofoldenable
-augroup END
-]])
+vim.cmd("autocmd BufEnter,BufRead *.md set spell wrap linebreak nofoldenable textwidth=100")
 -- vim.cmd("autocmd Filetype markdown LanguageToolSetUp")
 vim.cmd("autocmd BufRead,BufNewFile *.awf set ft=awf")
 vim.cmd("autocmd BufRead,BufNewFile,BufEnter *.rom set binary ft=xxd")
